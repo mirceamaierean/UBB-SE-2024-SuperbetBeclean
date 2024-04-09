fa3eb166e9de389f2e328f0bca7831b5dd615c86
HoriBossssssul <hori6c@gmail.com>
2024-04-10 01:37:58 +0300

    created all stored procedures for tables: Challenge, Request, (Poker)Table, Title, UserTitle

---
+++ db/queries/table.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE CreatePokerTable
+    @table_name VARCHAR(255),
+    @table_buyIn INT,
+    @table_playerLimit INT
+AS
+BEGIN
+    INSERT INTO PokerTable (table_name, table_buyIn, table_playerLimit)
+    VALUES (@table_name, @table_buyIn, @table_playerLimit)
+END
