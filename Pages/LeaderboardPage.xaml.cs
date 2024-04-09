87925b4587ce5b211ee14534b1ac6be04c6d81ee
dariuslacatus03 <dariusandrey2003@gmail.com>
2024-04-09 20:04:16 +0300

    designed the leaderboard

---
+++ Pages/LeaderboardPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SuperbetBeclean.Pages
+{
+    /// <summary>
+    /// Interaction logic for LeaderboardPage.xaml
+    /// </summary>
+    public partial class LeaderboardPage : Page
+    {
+        private Frame _mainFrame;
+        public LeaderboardPage(Frame mainFrame)
+        {
+            InitializeComponent();
+            _mainFrame = mainFrame;
+        }
+
+        private void onClickLeaderboardBack(object sender, RoutedEventArgs e)
+        {
+            _mainFrame.Navigate(new LobbyPage());
+        }
+    }
+}
