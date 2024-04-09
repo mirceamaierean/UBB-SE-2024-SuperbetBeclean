7542337b1055aa3c8e2636b5d08f84c5b4a15e1b
IchimVladAndrei <115074815+IchimVladAndrei@users.noreply.github.com>
2024-04-09 17:50:14 +0300

    partial-done

--- Pages/LobbyPage.xaml.cs
+++ Pages/LobbyPage.xaml.cs
@@ -11,5 +11,10 @@ namespace SuperbetBeclean.Pages
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
     }
 }
