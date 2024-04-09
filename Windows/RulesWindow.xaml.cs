a9d51933a4203a83305334319dfe62db86060623
manearobert3 <manea.robert.petrisor@gmail.com>
2024-04-09 21:21:39 +0300

    finished all rules page

--- Windows/RulesWindow.xaml.cs
+++ Windows/RulesWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,18 +12,51 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace SuperbetBeclean.Windows
 {
+    using System;
+    using System.IO;
+    using System.Windows;
     /// <summary>
     /// Interaction logic for RulesWindow.xaml
     /// </summary>
     public partial class RulesWindow : Window
     {
+        private const string HtmlFilePath = @".\assets\index.html";
+
         public RulesWindow()
         {
             InitializeComponent();
+            LoadHtmlContent();
         }
 
+        private void closeButtonRulesWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void LoadHtmlContent()
+        {
+            string solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+            // Combine the solution directory with the relative HTML file path
+            string htmlFilePath = Path.Combine(solutionDirectory, HtmlFilePath);
+
+
+
+            if (File.Exists(htmlFilePath))
+            {
+                // Read HTML content from the file
+                string htmlContent = File.ReadAllText(htmlFilePath);
+
+                // Display the HTML content in the WebBrowser control
+                WebBrowser.NavigateToString(htmlContent);
+            }
+            else
+            {
+                MessageBox.Show("Rules file not found.");
+            }
+        }
     }
 }
