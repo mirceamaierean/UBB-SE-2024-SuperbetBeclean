7d3c09c868351030f5c2dd1c8c8f2a8de4f5c032
dariuslacatus03 <dariusandrey2003@gmail.com>
2024-04-09 20:07:46 +0300

    forgot to save before last commit

--- Pages/LeaderboardPage.xaml.cs
+++ Pages/LeaderboardPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SuperbetBeclean.Pages
 {
@@ -29,7 +17,7 @@ namespace SuperbetBeclean.Pages
 
         private void onClickLeaderboardBack(object sender, RoutedEventArgs e)
         {
-            _mainFrame.Navigate(new LobbyPage());
+            _mainFrame.Navigate(new LobbyPage(_mainFrame));
         }
     }
 }
