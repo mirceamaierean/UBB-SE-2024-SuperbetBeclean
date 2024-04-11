13c25205db42e7cfc1c66a7ff3684481bea8560b
Dragos <dragos.ignat30@gmail.com>
2024-04-11 17:59:49 +0300

    Refactored code to keep style consistent

--- db/queries/table.sql
+++ db/queries/table.sql
@@ -1,9 +1,11 @@
-CREATE PROCEDURE CreatePokerTable
+CREATE OR ALTER PROCEDURE createPokerTable
     @table_name VARCHAR(255),
     @table_buyIn INT,
     @table_playerLimit INT
 AS
 BEGIN
-    INSERT INTO PokerTable (table_name, table_buyIn, table_playerLimit)
-    VALUES (@table_name, @table_buyIn, @table_playerLimit)
+    INSERT INTO PokerTable
+        (table_name, table_buyIn, table_playerLimit)
+    VALUES
+        (@table_name, @table_buyIn, @table_playerLimit)
 END
